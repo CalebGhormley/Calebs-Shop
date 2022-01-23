be1836b98bf0491a2bee20c71233fe1a61aa2880
CGhormley <CGhormley@vailresorts.com>
2022-01-22 23:30:20 -0700

    Improved reactive columns for product modification page

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { Category } from 'src/app/models/category';
@@ -11,19 +12,29 @@ import { ProductService } from 'src/app/service/product.service';
   templateUrl: './product-form.component.html',
   styleUrls: ['./product-form.component.scss']
 })
-export class ProductFormComponent implements OnInit {
+export class ProductFormComponent {
+  public mobileQuery: MediaQueryList;
+  private _screenQueryListener: () => void;
   public categories$: Observable<{ key:string | null, value:Category | null }[]>;
   public nameFormControl = new FormControl('', [Validators.required]);
   public priceFormControl = new FormControl('', [Validators.required, Validators.min(0)]);
   public categoryFormControl = new FormControl('', [Validators.required]);
   public imageUrlFormControl = new FormControl('', [Validators.required, Validators.pattern('(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?')]);
 
-  constructor(public categoryService: CategoryService,
+  constructor(public changeDetectorRef: ChangeDetectorRef,
+              public media: MediaMatcher,
+              public categoryService: CategoryService,
               private productService: ProductService,) { 
+    // Register screen event listener
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._screenQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addEventListener('change', this._screenQueryListener);
+    // Get categories
     this.categories$ = categoryService.getCategoriesWithKey$();
   }
 
-  ngOnInit(): void {
+  ngOnDestroy(): void {
+    this.mobileQuery.removeEventListener('change', this._screenQueryListener);
   }
 
   save(product: Product){
